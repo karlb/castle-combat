9adb2d9e8b8879ee1ffbefe751f4ca6e14ba14d1
Karl Bartel <karl42@gmail.com>
2019-09-18 10:58:00 +0200

    Fixes to run CC on a modern system
    Disabled sound for now due to crashes.

--- castle-combat.py
+++ castle-combat.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import sys
 sys.path.append('src')
 
-print "Castle-Combat requires pygame and twisted. If the game doesn't start up correctly, please verify that these are installed."
+print "Castle-Combat requires pygame and twisted. If the game doesn't start up correctly, please verify that these are installed in the versions given in requirements.txt."
 
 import main
 main.main()
